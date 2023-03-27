bd1e04ab2f6b8fe07c5250cc2921b2d94a4fef90
Arthur LAUNOY <alaunoy@consultants-solutec.fr>
2023-03-27 17:53:50 +0200

    user can now choose tile size from command line. added more images to source. still need to optimize program

--- README.md
+++ README.md
@@ -5,12 +5,11 @@ Learning the basics of image processing by implementing a basic photomosaics pro
 ## HOW TO USE
 
 Add all the images you want to see in the final results to the [source](Source_Images) folder. <br>
-Then run the `source_processing.py` script to standardize all the images. <br>
-Finally, run the `photomosaics.py` script to apply the magic !
+Simply run ``python photomosaics.py 'input' 'tile_size'``, with 'input' being the path to your input image and 'tile_size' being the precision you want to use(A low tile_size means a higher precision). Keep in mind that the lower 'tile_size' is, the longer the program will take to run.
 
 ## PHOTO CREDITS
 
 - Basics Images
     - All images were taken from this website : [http.cat](https://http.cat/)
 - Source Images
-    - Images taken from [Bla Bla Dataset](data.set) by Bla Bla
+    - Images taken from [T91 Dataset](https://www.kaggle.com/datasets/ll01dm/t91-image-dataset) on Kaggle.
--- photomosaics.py
+++ photomosaics.py
@@ -2,19 +2,18 @@ import cv2 as cv
 import numpy as np
 import os, sys
 
-TILE_SIZE = 25
 library, library_path = {}, "Source_Images"
 cache = {}
 
 
-def photomosaics(filename):
+def photomosaics(filename, tile_size):
     img = cv.imread(filename)
-    img = cv.resize(img, (img.shape[1] - (img.shape[1] % TILE_SIZE), img.shape[0] - (img.shape[0] % TILE_SIZE)))
+    img = cv.resize(img, (img.shape[1] - (img.shape[1] % tile_size), img.shape[0] - (img.shape[0] % tile_size)))
 
     for file_name in os.listdir(library_path):
         img_path = os.path.join(library_path, file_name)
         image = cv.imread(img_path)
-        library[file_name] = cv.resize(image, (TILE_SIZE, TILE_SIZE))
+        library[file_name] = cv.resize(image, (tile_size, tile_size))
     
     if not library:
         print("Error: Image library is empty.")
@@ -25,18 +24,24 @@ def photomosaics(filename):
     
     output_image = np.zeros_like(img)
 
-    for y in range(0, img.shape[1], TILE_SIZE):
-        for x in range(0, img.shape[0], TILE_SIZE):
-            tile = img[y:y+TILE_SIZE, x:x+TILE_SIZE]
+    for y in range(0, img.shape[0], tile_size):
+        for x in range(0, img.shape[1], tile_size):
+            tile = img[y:y+tile_size, x:x+tile_size]
             tile_avg_color = np.mean(tile, axis=(0, 1)).astype(int)
             best_match = min(cache, key=lambda x: np.linalg.norm(tile_avg_color - cache[x]))
             best_match_image = library[best_match]
-            output_image[y:y+TILE_SIZE, x:x+TILE_SIZE] = best_match_image
+            output_image[y:y+tile_size, x:x+tile_size] = best_match_image
     
     return output_image
 
 if __name__ == "__main__":
-    img = photomosaics(sys.argv[1])
-    cv.imshow('output Image', img)
+    img = photomosaics(sys.argv[1], int(sys.argv[2]))
+    img = cv.resize(img, (500, 500))
+    og = cv.imread(sys.argv[1])
+    og = cv.resize(og, (500, 500))
+    cv.imshow('Result', img)
+    cv.imshow('Input image', og)
+    cv.moveWindow('Input image', 100, 50)
+    cv.moveWindow('Result', 625, 50)
     cv.waitKey(0)
     cv.destroyAllWindows()
